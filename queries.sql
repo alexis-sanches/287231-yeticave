4d80aa3f51e68b266f4ba721656a93bc51dfd1ac
alexis-sanches <alexander.v.sannikov@gmail.com>
2017-09-20 08:39:01 +0300

    fix-module6-task2 Правки в запросы

--- queries.sql
+++ queries.sql
@@ -23,8 +23,8 @@ INSERT INTO lots
 VALUES
   ('2017-08-11 10:00:00', '2014 Rossignol District Snowboard', 'description', 'img/lot-1.jpg', 10999, '2017-10-11 00:00:00', 500, 10, 1, 3, 1),
   ('2017-09-12 11:10:00', 'DC Ply Mens 2016/2017 Snowboard', 'description', 'img/lot-2.jpg', 159999, '2017-10-01 00:00:00', 6000, 120, 2, 2, 1),
-  ('2017-07-29 12:20:00', 'Крепления Union Contact Pro 2015 года размер L/XL', 'description', 'img/lot-3.jpg', 8000, '2017-10-05 00:00:00', 250, 14, 3, 1, 2),
-  ('2017-09-14 13:30:00', 'Ботинки для сноуборда DC Mutiny Charocal', 'description', 'img/lot-4.jpg', 10999, '2017-10-22 00:00:00', 450, 10, 1, 3, 3),
+  ('2017-07-29 12:20:00', 'Крепления Union Contact Pro 2015 года размер L/XL', 'description', 'img/lot-3.jpg', 8000, '2017-10-05 00:00:00', 250, 14, 3, NULL , 2),
+  ('2017-09-14 13:30:00', 'Ботинки для сноуборда DC Mutiny Charocal', 'description', 'img/lot-4.jpg', 10999, '2017-10-22 00:00:00', 450, 10, 1, NULL, 3),
   ('2017-09-01 14:40:00', 'Куртка для сноуборда DC Mutiny Charocal', 'description', 'img/lot-5.jpg', 7500, '2017-10-30 00:00:00', 100, 10, 2, 2, 4),
   ('2017-09-17 15:50:00', 'Маска Oakley Canopy', 'description', 'img/lot-6.jpg', 5400, '2017-10-30 00:00:00', 50, 10, 3, 1, 6);
 
@@ -32,23 +32,22 @@ VALUES
 INSERT INTO bets
   (created_at, cost, user_id, lot_id)
 VALUES
-  ('2017-09-11 20:19:44', 11999, 3, 1),
-  ('2017-08-29 11:22:33', 11499, 2, 1);
+  ('2017-09-11 20:19:44', 11999, 3, 4),
+  ('2017-08-29 11:22:33', 11499, 2, 4);
 
 /* Получить список из всех категорий */
 SELECT * FROM categories;
 
 /* Получить самые новые, открытые лоты */
-SELECT l.id, l.title, price, image_url, c.title
+SELECT l.id, l.title, price, image_url, COALESCE(MAX(cost), price) AS current_price, COUNT(cost) AS bet_cnt, c.title AS cat_title
 FROM lots l
-  JOIN categories c
-    ON l.category_id = c.id
+  JOIN categories c ON l.category_id = c.id
+  LEFT JOIN bets b ON l.id = b.lot_id
 WHERE winner_id IS NULL
-ORDER BY created_at DESC;
+GROUP BY l.id ORDER BY l.created_at DESC;
 
 /* Найти лот по его названию или описанию */
-SELECT * FROM lots WHERE title LIKE 'Rossignol';
-SELECT * FROM lots WHERE description LIKE 'Rossignol';
+SELECT * FROM lots WHERE title LIKE '%Rossignol%' OR description LIKE '%Rossignol%';
 
 /* Обновить название лота по его идентификатору */
 UPDATE lots SET title ='New Title' WHERE id = 1;
